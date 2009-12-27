8e7f4dac31692b9b3093ff3e5c743847359f4183
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-27 12:30:48 -0800

    Oops, moves are randomized and so should be an IO action, not a pure function!

--- sources/VPI/Fortran/Path/Moves.hs
+++ sources/VPI/Fortran/Path/Moves.hs
@@ -45,9 +45,9 @@ foreign import ccall unsafe "vpi__path__moves__rigid" vpi__path__moves__rigid ::
     Ptr (Double) -> -- new particle positions
     IO ()
 
-rigid :: Int -> Double -> Array3D Double -> Array3D Double
+rigid :: Int -> Double -> Array3D Double -> IO (Array3D Double)
 rigid particle_number maximum_shift old_particle_positions =
-    fst . unsafePerformIO $
+    fmap fst $
     withNDArray old_particle_positions $ \p_old_particle_positions ->
     withNewNDArray (ndarrayShape old_particle_positions) $ \p_new_particle_positions ->
         vpi__path__moves__rigid
