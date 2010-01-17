137c99cc972df71e9384eab9698888ba47002e89
Gregory Crosswhite <gcross@phys.washington.edu>
2010-01-16 19:45:11 -0800

    Added a friendlier interface to the brownian bridge move generator.

--- sources/VPI/Path/Moves.hs
+++ sources/VPI/Path/Moves.hs
@@ -13,6 +13,7 @@ module VPI.Path.Moves where
 -- @+node:gcross.20100114153410.1569:<< Import needed modules >>
 import VPI.Fortran.Path.Moves
 import VPI.Path
+import VPI.Spliceable
 -- @-node:gcross.20100114153410.1569:<< Import needed modules >>
 -- @nl
 
@@ -29,6 +30,41 @@ rigidMove maximum_shift particle_number (Path old_positions old_separations) =
                 new_positions
                 (updateSeparationsForParticle particle_number old_positions old_separations)
 -- @-node:gcross.20100114153410.1571:rigidMove
+-- @+node:gcross.20100116114537.1623:brownianBridgeMove
+brownianBridgeMove :: Double -> Double -> Int -> Int -> Int -> Path -> IO (Int,Path)
+brownianBridgeMove
+    hbar_over_2m time_interval
+    particle_number
+    start_slice end_slice
+    old_path
+    =
+    let number_of_slices = pathNumberOfSlices old_path
+        (start_of_cut,move_leftmost_slice) =
+            if start_slice <= 0
+                then (0,True)
+                else (start_slice-1,False)
+        (end_of_cut,move_rightmost_slice) =
+            if end_slice >= number_of_slices
+                then (number_of_slices,True)
+                else (end_slice+1,False)
+        Path old_positions old_separations =
+            subrange start_of_cut end_of_cut old_path
+    in
+    brownian_bridge
+        hbar_over_2m time_interval
+        (particle_number+1)
+        move_leftmost_slice
+        move_rightmost_slice
+        old_positions
+    >>=
+    \new_positions ->
+        return $
+            (start_of_cut
+            ,Path
+                new_positions
+                (updateSeparationsForParticle particle_number old_positions old_separations)
+            )
+-- @-node:gcross.20100116114537.1623:brownianBridgeMove
 -- @-node:gcross.20100114153410.1570:Functions
 -- @-others
 -- @-node:gcross.20100107114651.1444:@thin Moves.hs
